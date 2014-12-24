5193538faaa58d8d7ba55ff2a15e5e2b6f4c66e8
Zemoj <jeffgomez@zemoj.com>
2014-12-23 19:51:21 -0800

    Update ChatAddonAntiSpam.java
    Better handling of multiple simultaneous spam messages.

--- src/main/java/mnm/mods/tabbychat/ChatAddonAntiSpam.java
+++ src/main/java/mnm/mods/tabbychat/ChatAddonAntiSpam.java
@@ -1,6 +1,8 @@
 package mnm.mods.tabbychat;
 
+import java.util.List;
 import mnm.mods.tabbychat.api.TabbyAPI;
+import mnm.mods.tabbychat.api.Message;
 import mnm.mods.tabbychat.api.listener.ChatRecievedListener;
 import mnm.mods.tabbychat.api.listener.events.AddonInitEvent;
 import mnm.mods.tabbychat.api.listener.events.ChatMessageEvent.ChatRecievedEvent;
@@ -11,6 +13,7 @@ public class ChatAddonAntiSpam implements ChatRecievedListener {
 
     private String lastMessage = "";
     private int spamCounter = 1;
+    private int lineCounter = 1;
 
     @Override
     public void initAddon(AddonInitEvent init) {
@@ -19,13 +22,25 @@ public class ChatAddonAntiSpam implements ChatRecievedListener {
     @Override
     public void onChatRecievedFilter(ChatRecievedFilterEvent message) {
         if (TabbyChat.getInstance().generalSettings.antiSpam.getValue() && message.id == 0) {
-            IChatComponent chat = message.chat;
-            if (!this.lastMessage.isEmpty() && chat.getUnformattedText().equals(this.lastMessage)) {
-                spamCounter++;
-                chat.appendText(" [" + spamCounter + "x]");
-                TabbyAPI.getAPI().getChat().removeMessageAt(1);
+
+			IChatComponent chat = message.chat;
+            List<Message> messages = TabbyAPI.getAPI().getChat().getMessages();
+			
+            if (!this.lastMessage.isEmpty() && chat.getUnformattedText().equals(this.lastMessage)) {	
+                if(lineCounter == 1){
+                    do{
+                        spamCounter++;
+                        lineCounter++;
+                        TabbyAPI.getAPI().getChat().removeMessageAt(1);
+                        messages = TabbyAPI.getAPI().getChat().getMessages();
+                    }while(messages.get(1).getMessage().getUnformattedText().equals(this.lastMessage));				
+                    chat.appendText(" [" + spamCounter + "x]");		
+                }else{
+                    lineCounter--;
+                }
             } else {
                 spamCounter = 1;
+                lineCounter = 1;
                 this.lastMessage = chat.getUnformattedText();
             }
         }
