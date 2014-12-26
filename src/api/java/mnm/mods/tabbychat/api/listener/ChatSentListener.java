1f579c8d024b9e7b2e39a8475c40dcf6c7e58282
killjoy1221 <mattmess1221@gmail.com>
2014-12-26 01:46:39 -0500

    Merge chat listeners. No more distinction with filter.

--- src/api/java/mnm/mods/tabbychat/api/listener/ChatSentListener.java
+++ src/api/java/mnm/mods/tabbychat/api/listener/ChatSentListener.java
@@ -1,11 +1,8 @@
 package mnm.mods.tabbychat.api.listener;
 
 import mnm.mods.tabbychat.api.listener.events.ChatMessageEvent.ChatSentEvent;
-import mnm.mods.tabbychat.api.listener.events.ChatMessageEvent.ChatSentFilterEvent;
 
 public interface ChatSentListener extends TabbyListener {
 
     void onChatSent(ChatSentEvent sendEvent);
-
-    void onChatSentFilter(ChatSentFilterEvent sendEvent);
 }
--- src/api/java/mnm/mods/tabbychat/api/listener/events/ChatMessageEvent.java
+++ src/api/java/mnm/mods/tabbychat/api/listener/events/ChatMessageEvent.java
@@ -14,7 +14,7 @@ public abstract class ChatMessageEvent extends Event {
      */
     public static class ChatSentEvent extends ChatMessageEvent {
 
-        public final String message;
+        public String message;
 
         public ChatSentEvent(String message) {
             this.message = message;
@@ -22,42 +22,16 @@ public abstract class ChatMessageEvent extends Event {
     }
 
     /**
-     * Filters the messages when they are sent.
-     */
-    public static class ChatSentFilterEvent extends ChatMessageEvent {
-
-        public String message;
-
-        public ChatSentFilterEvent(String message) {
-            this.message = message;
-        }
-    }
-
-    /**
-     * Used to listen to chat and assign it a number of channels. Chat and
-     * position cannot be changed. Use {@link ChatRecievedFilterEvent} instead.
+     * Used to listen to chat and modify it. Can also select which channels it
+     * goes to.
      */
     public static class ChatRecievedEvent extends ChatMessageEvent {
 
-        public final IChatComponent chat;
-        public final int id;
-        public final List<Channel> channels = Lists.newArrayList();
-
-        public ChatRecievedEvent(IChatComponent chat, int id) {
-            this.chat = chat;
-            this.id = id;
-        }
-    }
-
-    /**
-     * Used to change or move chat.
-     */
-    public static class ChatRecievedFilterEvent extends ChatMessageEvent {
-
         public IChatComponent chat;
         public int id;
+        public List<Channel> channels = Lists.newArrayList();
 
-        public ChatRecievedFilterEvent(IChatComponent chat, int id) {
+        public ChatRecievedEvent(IChatComponent chat, int id) {
             this.chat = chat;
             this.id = id;
         }
