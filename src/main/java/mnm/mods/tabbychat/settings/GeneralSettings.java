ae5438d0916e8bf84b571d64ae7db8ab4096b386
Zemoj <jeffgomez@zemoj.com>
2015-02-22 21:00:02 -0800

    Update GeneralSettings.java

--- src/main/java/mnm/mods/tabbychat/settings/GeneralSettings.java
+++ src/main/java/mnm/mods/tabbychat/settings/GeneralSettings.java
@@ -1,11 +1,9 @@
 package mnm.mods.tabbychat.settings;
 
-import mnm.mods.tabbychat.util.FormattingSerializer;
 import mnm.mods.tabbychat.util.TimeStamps;
+import mnm.mods.tabbychat.util.Translation;
 import mnm.mods.util.SettingValue;
 import net.minecraft.util.EnumChatFormatting;
-
-import com.google.gson.GsonBuilder;
 
 public class GeneralSettings extends TabbySettings {
 
@@ -17,11 +15,13 @@ public class GeneralSettings extends TabbySettings {
     public SettingValue<EnumChatFormatting> timestampColor = new SettingValue<EnumChatFormatting>(
             EnumChatFormatting.WHITE);
     public SettingValue<Boolean> antiSpam = new SettingValue<Boolean>(false);
+    public SettingValue<Boolean> antiSpamPartial = new SettingValue<Boolean>(true);
+    public SettingValue<Float> antiSpamPartialAmount = new SettingValue<Float>(0.08f);    
     public SettingValue<Boolean> unreadFlashing = new SettingValue<Boolean>(true);
     public SettingValue<Boolean> checkUpdates = new SettingValue<Boolean>(true);
 
     public GeneralSettings() {
-        super("general");
+        super(Translation.SETTINGS_GENERAL.translate());
 
         registerSetting("logChat", logChat);
         registerSetting("splitLog", splitLog);
@@ -29,12 +29,9 @@ public class GeneralSettings extends TabbySettings {
         registerSetting("timestampStyle", timestampStyle);
         registerSetting("timestampColor", timestampColor);
         registerSetting("antiSpam", antiSpam);
+        registerSetting("antiSpamPartial", antiSpamPartial);
+        registerSetting("antiSpamPartialAmount", antiSpamPartialAmount);
         registerSetting("unreadFlashing", unreadFlashing);
         registerSetting("checkUpdates", checkUpdates);
-    }
-
-    @Override
-    protected void setupGson(GsonBuilder builder) {
-        builder.registerTypeAdapter(EnumChatFormatting.class, new FormattingSerializer());
     }
 }
