d5686ed794bcb7e48051f16f40f1812e709d65e7
Matthew Messinger <mattmess1221@gmail.com>
2019-04-03 21:41:43 -0400

    Add a simple network event. Server side not implemented yet.

--- src/main/java/mnm/mods/tabbychat/TabbyChat.java
+++ src/main/java/mnm/mods/tabbychat/TabbyChat.java
@@ -1,11 +1,17 @@
 package mnm.mods.tabbychat;
 
 import mnm.mods.tabbychat.client.TabbyChatClient;
+import mnm.mods.tabbychat.net.SSendChannelMessage;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
+import net.minecraftforge.fml.network.NetworkRegistry;
+import net.minecraftforge.fml.network.simple.SimpleChannel;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -20,11 +26,36 @@ public class TabbyChat {
 
     public static final Path dataFolder = FMLPaths.CONFIGDIR.get().resolve(MODID);
 
+    private static final String PROTOCL_VERSION = "1.0";
+
+    public static final SimpleChannel channel = NetworkRegistry.ChannelBuilder
+            .named(new ResourceLocation(TabbyChat.MODID, "channel"))
+            .networkProtocolVersion(() -> PROTOCL_VERSION)
+            // TODO not required on either side
+            .clientAcceptedVersions(PROTOCL_VERSION::equals)
+            .serverAcceptedVersions(PROTOCL_VERSION::equals)
+            .simpleChannel();
+
     public TabbyChat() {
-        DistExecutor.runWhenOn(Dist.CLIENT, () -> TabbyChat::initclient);
+        DistExecutor.runWhenOn(Dist.CLIENT, () -> TabbyChat::initClient);
     }
 
-    private static void initclient() {
+    @SubscribeEvent
+    public void init(FMLCommonSetupEvent event) {
+        initNetwork();
+    }
+
+    private void initNetwork() {
+        int id = 0;
+
+        channel.messageBuilder(SSendChannelMessage.class, id++)
+                .encoder(SSendChannelMessage::encode)
+                .decoder(SSendChannelMessage::new)
+                .consumer(SSendChannelMessage::handle)
+                .add();
+    }
+
+    private static void initClient() {
         FMLJavaModLoadingContext.get().getModEventBus().register(TabbyChatClient.getInstance());
     }
 
---
+++ src/main/java/mnm/mods/tabbychat/net/SSendChannelMessage.java
@@ -0,0 +1,49 @@
+package mnm.mods.tabbychat.net;
+
+import mnm.mods.tabbychat.TCMarkers;
+import mnm.mods.tabbychat.TabbyChat;
+import mnm.mods.tabbychat.api.Channel;
+import mnm.mods.tabbychat.client.ChatManager;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraftforge.fml.network.NetworkEvent;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public class SSendChannelMessage {
+
+
+    private String channel;
+    private ITextComponent message;
+
+    public SSendChannelMessage(PacketBuffer buffer) {
+        channel = buffer.readString(20);
+        message = buffer.readTextComponent();
+    }
+
+    public SSendChannelMessage(String channel, ITextComponent message) {
+        this.message = message;
+    }
+
+    public void encode(PacketBuffer buffer) {
+        buffer.writeString(channel);
+        buffer.writeTextComponent(message);
+    }
+
+    public void handle(Supplier<NetworkEvent.Context> context) {
+        context.get().enqueueWork(() -> {
+            ChatManager chat = ChatManager.instance();
+            Optional<Channel> chan = chat.parseChannel(channel);
+            if (!chan.isPresent()) {
+                TabbyChat.logger.warn(TCMarkers.NETWORK, "Server sent bad channel name: {}", channel);
+                return;
+            }
+
+            chat.addMessage(chan.get(), message);
+        });
+        context.get().setPacketHandled(true);
+    }
+}
