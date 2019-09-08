dd16e356910d0c1a583d8bf1d0e842645b5b0d61
Matthew Messinger <mattmess1221@gmail.com>
2019-09-08 18:02:17 -0400

    Revert a change that made configs not save and load properly.

--- src/main/kotlin/mnm/mods/tabbychat/util/config/SettingsFile.kt
+++ src/main/kotlin/mnm/mods/tabbychat/util/config/SettingsFile.kt
@@ -22,7 +22,7 @@ import java.nio.file.Path
 abstract class SettingsFile<T : SettingsFile<T>>(@field:Transient val path: Path) : ValueObject<SettingsFile<T>>() {
 
     private val gson = GsonBuilder()
-            .registerTypeAdapter(SettingsFile::class.java, Serializer())
+            .registerTypeAdapter(this::class.java, Serializer())
             .setPrettyPrinting()
             .registerTypeAdapterFactory(EnumTypeAdapterFactory())
             .create()
